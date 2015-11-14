8513fd9b8e68cd1de0a8496ddc7a7998870f5567
Fabio Manganiello <blacklight86@gmail.com>
2015-11-14 16:38:12 +0100

    We have removed IDs as mandatory fields for events - fixing tests

--- tests/test_event.py
+++ tests/test_event.py
@@ -7,24 +7,21 @@ from evesp.event import Event
 
 class TestEvent(unittest.TestCase):
     def setUp(self):
-        self.evt = Event(1, foo='bar')
+        self.evt = Event(foo='bar')
 
     def test_event_creation(self):
-        self.assertEqual(self.evt.id(), 1)
         self.assertEqual(self.evt.get('foo'), 'bar')
         self.assertEqual(self.evt.get('not-existing'), None)
 
     def test_event_pickle_serialization(self):
         ser_evt = self.evt.serialize()
         deser_evt = Event.deserialize(ser_evt)
-        self.assertEqual(deser_evt.id(), 1)
         self.assertEqual(deser_evt.get('foo'), 'bar')
         self.assertEqual(deser_evt.get('not-existing'), None)
 
     def test_event_json_serialization(self):
         ser_evt = self.evt.to_json()
         deser_evt = Event.from_json(ser_evt)
-        self.assertEqual(deser_evt.id(), 1)
         self.assertEqual(deser_evt.get('foo'), 'bar')
         self.assertEqual(deser_evt.get('not-existing'), None)
 
