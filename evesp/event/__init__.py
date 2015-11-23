c4112f06512dca0a72d6d4852c6a50489eb04925
Fabio Manganiello <blacklight86@gmail.com>
2015-11-23 20:08:32 +0100

    Logic for comparing events

--- evesp/event/__init__.py
+++ evesp/event/__init__.py
@@ -13,8 +13,7 @@ class Event(object):
 
         self.__kwargs = kwargs
         self.component = component
-        for attr, value in kwargs.items():
-            self.__dict__[attr] = value
+        vars(self).update(kwargs)
 
     def get(self, attr):
         " Get an event attribute by name. Return None if the attribute doesn't exist "
@@ -46,6 +45,48 @@ class Event(object):
         import json
         attrs = dict(json.loads(attrs))
         return Event(**attrs)
+
+    def __eq__(self, event):
+        """
+        Return true if event equals self.
+        Two events are considered "equal" if:
+
+        - Their types are the same, or one is a direct subclass of the other;
+        - All of their constructor parameters are equal, unless a certain attribute is an instance of AttributeValueAny.
+        """
+
+        if not self.__same_classes(self, event):
+            return False
+
+        for (attr, value) in self.__kwargs.items():
+            if not self.__same_values(value, event.__kwargs[attr]):
+                return False
+        return True
+
+    @classmethod
+    def __same_classes(cls, obj1, obj2):
+        return type(obj1) == obj2.__class__
+
+    @classmethod
+    def __same_values(cls, value1, value2):
+        if not cls.__same_classes(value1, value2) \
+                and not isinstance(value1, AttributeValueAny) \
+                and not isinstance(value2, AttributeValueAny):
+            return False
+        return value1 == value2
+
+class AttributeValueAny(object):
+    """
+    When an event attribute type is AttributeValueAny,
+    that attribute won't be taken into account when
+    .equals is invoked to compare it with another event.
+
+    Fabio Manganiello, 2015 <blacklight86@gmail.com>
+    """
+
+    def __eq__(self, value):
+        """ Always return True. Any value equals "any" """
+        return True
 
 # vim:sw=4:ts=4:et:
 
