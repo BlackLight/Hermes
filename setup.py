5e2fd29091dd6289530f4beeaa1c947eb5ad5f26
Fabio Manganiello <blacklight86@gmail.com>
2015-11-23 20:09:07 +0100

    Forgot that debug print

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 conf_file_path = os.path.join(os.getenv('HOME'), '.config', 'evesp')
-print(conf_file_path)
 
 setup(
     name = "evesp",
