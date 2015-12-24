8b4e8440942cbcf3f72f57b0c66b0f73c3b06cc1
Fabio Manganiello <blacklight86@gmail.com>
2015-12-24 01:56:03 +0100

    s/file_event/file_system_event/, since Inotify does not apply only to files

---
+++ evesp/event/file_system_event/__init__.py
@@ -0,0 +1,22 @@
+from evesp.event import Event
+
+class FileSystemEvent(Event):
+    """
+    Class for mapping file system events - e.g. file, directory or block
+    devices creation, removal, access and modification.
+
+    Fabio Manganiello, 2015 <blacklight86@gmail.com>
+    """
+
+    def __init__(self, path, mask, **kwargs):
+        """
+        path -- Path name that triggered the event
+        mask -- Event mask
+        """
+
+        super().__init__(kwargs)
+        self.path = path
+        self.mask = mask
+
+# vim:sw=4:ts=4:et:
+
