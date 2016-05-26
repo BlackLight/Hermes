4a0d7f5f15a220e741169ca6d070295824f6a98f
Fabio Manganiello <blacklight86@gmail.com>
2016-05-27 01:49:32 +0200

    Tests for file system event components

--- tests/test_file_system_event_component.py
+++ tests/test_file_system_event_component.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import sys
 import threading
 import time
 import unittest
@@ -10,35 +9,46 @@ from evesp.engine import Engine
 from evesp.event.file_system_event import FileSystemEvent
 from evesp.component.file_system_event_component import FileSystemEventComponent
 
+
 class TestFileSystemEventComponent(unittest.TestCase):
     comp_name = 'File system event component'
     event_bin_file = os.path.join('tests', 'events.bin')
     watched_file = os.path.join('tests', 'mock_file')
+    db_file = os.path.join('tests', 'main.db')
 
     def __create_mock_file(self):
-        with open(self.watched_file, 'w') as fp: fp.close()
+        with open(self.watched_file, 'w') as fp:
+            fp.write('original content')
+
+    def __cleanup(self):
+        for f in [self.event_bin_file, self.watched_file, self.db_file]:
+            try:
+                os.unlink(f)
+            except FileNotFoundError:
+                pass
 
     def setUp(self):
-        try:
-            os.unlink(self.event_bin_file)
-            os.unlink(self.watched_file)
-        except FileNotFoundError:
-            pass
-
+        self.__cleanup()
         self.__create_mock_file()
 
         # Synchronize on this event to wait the engine stop
         self.engine_stopped = threading.Event()
 
         basedir = os.path.dirname(os.path.realpath(__file__))
-        config_file = os.path.join(basedir, 'conf', 'test_file_system_event_component.conf')
+        config_file = os.path.join(
+            basedir, 'conf', 'test_file_system_event_component.conf'
+        )
 
-        self.engine = Engine( config = Config(config_file), atexit_callback = self.__on_engine_exit )
-        threading.Thread(target = self.engine.start).start()
+        self.engine = Engine(
+            config=Config(config_file),
+            atexit_callback=self.__on_engine_exit
+        )
+
+        threading.Thread(target=self.engine.start).start()
 
         self.engine.wait_running()
-        time.sleep(0.2)
-        threading.Thread(target = self.__create_mock_file).start()
+        time.sleep(0.1)
+        threading.Thread(target=self.__create_mock_file).start()
 
     def __on_engine_exit(self):
         self.engine_stopped.set()
@@ -57,23 +67,17 @@ class TestFileSystemEventComponent(unittest.TestCase):
                 try:
                     evt = pickle.load(fp)
                     events.append(evt)
-                except EOFError: break
+                except EOFError:
+                    break
 
         self.assertEqual(len(events), 1)
         evt = events[0]
         self.assertTrue(isinstance(evt, FileSystemEvent))
-        # self.assertEqual(evt.id, 1)
-        # self.assertEqual(evt.name, 'Test event')
 
     def tearDown(self):
-        try:
-            os.unlink(self.event_bin_file)
-            os.unlink(self.watched_file)
-        except FileNotFoundError:
-            pass
+        self.__cleanup()
 
 if __name__ == "__main__":
     unittest.main()
 
 # vim:sw=4:ts=4:et:
-
